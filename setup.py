7a76c159e96b72269ca5866cccf2f1d87dcb6c3c
Adam Heins <mail@adamheins.com>
2021-01-16 18:56:25 -0500

    Add more info to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup
 
-install_requires = ['docopt']
+
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(name='t',
       version='0.1',
       description='CLI tool for removing files safely.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/adamheins/t',
       author='Adam Heins',
       author_email='mail@adamheins.com',
       install_requires=['docopt'],
       license='MIT',
       scripts=['t'],
-      python_requires='>=3',
+      python_requires='>=3.5',
       zip_safe=False)
